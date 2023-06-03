c66baed64de509bae2060a893e468a58eb9b9bee
mj1999 <mayank.joshi92@yahoo.com>
2023-06-03 14:05:27 +0530

    Add files via upload
    contact list using EJS view engine, using which we can add or delete contacts from our list

---
+++ ContactsList/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const port = 8000;
+const app = express();
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'view'));
+app.use(express.urlencoded());
+app.use(express.static("assets"))
+app.get('/',function(req,res){
+    res.render('home',{title:"My contacts list",contact_list:contacts});
+})
+
+app.post('/add-contact',function(req,res){
+    contacts.push(req.body);
+    res.redirect('back');
+})
+app.get('/delete-number/:phone',function(req,res){
+    const phoneObj = req.params;
+    for(let index in contacts)
+    {
+        
+        if(contacts[index].phone===phoneObj.phone)
+        {
+            contacts.splice(index,1);
+        }
+       
+    }
+    
+    res.redirect('/')
+})
+
+const contacts =[
+    {
+        name: 'Sarah', phone: '3435334456'
+    },
+    {
+        name: 'Jane', phone: '5142967395'
+    },
+    {
+        name: 'Jane', phone: '3716865393'
+    },
+    {name: 'Michael', phone: '2897515903'},
+    {name: 'Emily', phone: '9286990791'}
+]
+app.listen(port,function(err){
+    if(err)
+    {
+        console.log("err in server");
+    }
+    console.log("Server running");
+})
