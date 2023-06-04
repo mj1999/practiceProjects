26f1027cb3aba97ba7c9961cbb9b9146390b530c
mj1999 <mayank.joshi92@yahoo.com>
2023-06-04 14:07:35 +0530

    Add files via upload
    added mongodb database support to the server

--- ContactsList/index.js
+++ ContactsList/index.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const path = require('path');
 const port = 8000;
+const db = require('./config/mongoose');
+const Contact = require('./model/contactSchema');
+const { render } = require('ejs');
 const app = express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'view'));
-app.use(express.urlencoded());
+app.use(express.urlencoded()); //to decode form data and store in body property of req 
 app.use(express.static("assets"))
 app.get('/',function(req,res){
-    res.render('home',{title:"My contacts list",contact_list:contacts});
+    Contact.find().then((contacts)=>{res.render('home',{title:"My Contacts",contact_list:contacts})}).catch((err)=>{console.log(err,'error fetching contacts')});
+
 })
 
 app.post('/add-contact',function(req,res){
-    contacts.push(req.body);
+    Contact.create(req.body).then((contactDetail)=>{console.log('contact created successfully',contactDetail)}).catch((err)=>{console.log('error creating contact')});
     res.redirect('back');
 })
-app.get('/delete-number/:phone',function(req,res){
-    const phoneObj = req.params;
-    let index = contacts.findIndex((contact)=>contact.phone==phoneObj.phone);
-    
-    contacts.splice(index,1);
-    
-    res.redirect('/')
+app.get('/delete-number',function(req,res){
+    const id = req.query.id;
+    console.log(id);
+    Contact.findByIdAndDelete(id).then(()=>{res.redirect('/')}).catch(()=>{console.log('Error while deleting contact')});
+   
 })
 
 const contacts =[
